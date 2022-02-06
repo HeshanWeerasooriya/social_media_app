0ae412ecf53a6a48969451f2c6ea1632a62e912a
Heshan <heshan.imedha@gmail.com>
2022-02-06 17:37:29 +0530

    Displaying Activity Feed Notifications

--- lib/pages/activity_feed.dart
+++ lib/pages/activity_feed.dart
@@ -1,10 +1,162 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import '../widgets/header.dart';
+import '../widgets/progress.dart';
+import 'home.dart';
 
-class ActivityFeed extends StatelessWidget {
-  const ActivityFeed({Key? key}) : super(key: key);
+class ActivityFeed extends StatefulWidget {
+  @override
+  _ActivityFeedState createState() => _ActivityFeedState();
+}
+
+class _ActivityFeedState extends State<ActivityFeed> {
+  getActivityFeed() async {
+    QuerySnapshot snapshot = await activityFeedRef
+        .doc(currentUser!.id)
+        .collection('feedItems')
+        .orderBy('timestamp', descending: true)
+        .limit(50)
+        .get();
+    List<ActivityFeedItem> feedItems = [];
+    snapshot.docs.forEach((doc) {
+      feedItems.add(ActivityFeedItem.fromDocument(doc));
+      // print('Activity Feed Item: ${doc.data}');
+    });
+    return feedItems;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      appBar: header(context, titleText: "Activity Feed"),
+      body: FutureBuilder<dynamic>(
+        future: getActivityFeed(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return circularProgress();
+          }
+          return ListView(
+            children: snapshot.data,
+          );
+        },
+      ),
+    );
+  }
+}
+
+Widget? mediaPreview;
+String? activityItemText;
+
+class ActivityFeedItem extends StatelessWidget {
+  final String username;
+  final String userId;
+  final String type; // 'like', 'follow', 'comment'
+  final String mediaUrl;
+  final String postId;
+  final String userProfileImg;
+  final String commentData;
+  final Timestamp timestamp;
+
+  ActivityFeedItem({
+    required this.username,
+    required this.userId,
+    required this.type,
+    required this.mediaUrl,
+    required this.postId,
+    required this.userProfileImg,
+    required this.commentData,
+    required this.timestamp,
+  });
+
+  factory ActivityFeedItem.fromDocument(DocumentSnapshot doc) {
+    return ActivityFeedItem(
+      username: doc['username'],
+      userId: doc['userId'],
+      type: doc['type'],
+      postId: doc['postId'],
+      userProfileImg: doc['userProfileImg'],
+      commentData: doc['commentData'],
+      timestamp: doc['timestamp'],
+      mediaUrl: doc['mediaUrl'],
+    );
+  }
+
+  configureMediaPreview() {
+    if (type == "like" || type == 'comment') {
+      mediaPreview = GestureDetector(
+        onTap: () => print('showing post'),
+        child: Container(
+          height: 50.0,
+          width: 50.0,
+          child: AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: CachedNetworkImageProvider(mediaUrl),
+                  ),
+                ),
+              )),
+        ),
+      );
+    } else {
+      mediaPreview = Text('');
+    }
+
+    if (type == 'like') {
+      activityItemText = "liked your post";
+    } else if (type == 'follow') {
+      activityItemText = "is following you";
+    } else if (type == 'comment') {
+      activityItemText = 'replied: $commentData';
+    } else {
+      activityItemText = "Error: Unknown type '$type'";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    configureMediaPreview();
+
+    return Padding(
+      padding: EdgeInsets.only(bottom: 2.0),
+      child: Container(
+        color: Colors.white54,
+        child: ListTile(
+          title: GestureDetector(
+            onTap: () => print('show profile'),
+            child: RichText(
+              overflow: TextOverflow.ellipsis,
+              text: TextSpan(
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: username,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: ' $activityItemText',
+                    ),
+                  ]),
+            ),
+          ),
+          leading: CircleAvatar(
+            backgroundImage: CachedNetworkImageProvider(userProfileImg),
+          ),
+          subtitle: Text(
+            timeago.format(timestamp.toDate()),
+            overflow: TextOverflow.ellipsis,
+          ),
+          trailing: mediaPreview,
+        ),
+      ),
+    );
   }
 }
